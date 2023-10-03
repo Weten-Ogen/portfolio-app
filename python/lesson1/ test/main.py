b23ce4d2427096e5acecf963f7bf196a17ec0a0d
Weten-Ogen <alphaoware@gmail.com>
2023-10-03 11:15:06 +0000

    first lesson

---
+++ python/lesson1/ test/main.py
@@ -0,0 +1,9 @@
+# what is the type for the variable home_address
+home_address = '0123 - 506'
+
+# home_address is a variable and it is name refrences for storing your data in the computers memory
+
+bicycle = []
+# what is the type of bicycle 
+# i want you to build an dict with name Person , 
+# that has name,school and number fields.. from cs50 .. you can transfer the knowledge by refering to a python course . 
---
+++ python/lesson1/example/main.py
@@ -0,0 +1,20 @@
+# Variable and Built in Types .
+# DataTypes are the format in which data(what you are inputing into the computer , be it images,videos, numbers strings are stored).
+# Now we going to look are some built in DataTypes and why you know to know them and how different it is from other programming languages
+# ///
+# 
+# 
+# 
+#  Datatypes in python include int,float ,str, bool, list ,tuples ,dict, sets.
+# . 
+
+# i know you now know about variable declarations and asssignment 
+home_address =  "BLK 58" #string type 
+phone_number_count = 10  # this the int type
+are_you_a_male = True  # this a boolean type or bool answers the question yes or no it can only be True or False or values that leads to true or false
+are_you_sure_you_not_a_male= not True 
+# this could also be 
+are_you_sure_you_not_a_male = False
+
+
+# Go to the test session and try to answer the questions
